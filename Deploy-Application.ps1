d448341617b62e27f5ca322fa298e998477e372f
dtsyseng <65776544+dtsyseng@users.noreply.github.com>
2021-02-08 17:33:26 -0700

    Update Deploy-Application.ps1

--- Deploy-Application.ps1
+++ Deploy-Application.ps1
@@ -193,9 +193,9 @@ Try {
 		}
 
 		# <Perform Uninstallation tasks here>
-		##Execute-MSI -Action Uninstall -Path '{F4ABABFA-2281-4778-A920-B04BB07C2DBC}'
-		$exitCode = Execute-Process -Path "$dirFiles\rss1089n09s.exe" -Parameters "/s /a /s /f1c:\uninstall.iss" -WindowStyle "Hidden" -PassThru
-        If (($exitCode.ExitCode -ne "0") -and ($mainExitCode -ne "3010")) { $mainExitCode = $exitCode.ExitCode }
+		Execute-MSI -Action Uninstall -Path '{FCC74B77-EC3E-4DD8-008A702075A9}'
+		##$exitCode = Execute-Process -Path "$dirFiles\rss1089n09s.exe" -Parameters "/s /a /s /f1c:\uninstall.iss" -WindowStyle "Hidden" -PassThru
+        ##If (($exitCode.ExitCode -ne "0") -and ($mainExitCode -ne "3010")) { $mainExitCode = $exitCode.ExitCode }
 
         Remove-File -Path "C:\install.iss"
         Remove-File -Path "C:\uninstall.iss"
